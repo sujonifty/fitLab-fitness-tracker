2d51dd27c9487dc3f2601347ee4ca06aba147e84
sujonifty <sujonifty@gmail.com>
2024-06-03 11:56:49 +0600

    completed trainer application section

--- src/Pages/BeTrainer/BeTrainer.jsx
+++ src/Pages/BeTrainer/BeTrainer.jsx
@@ -1,12 +1,43 @@
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { AuthContext } from "../../Provider/AuthProvider";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 
 const BeTrainer = () => {
     const { user } = useContext(AuthContext);
+    const axiosPublic= useAxiosPublic();
     const handleBeTrainer = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const photo = e.target.photo.value;
+        const age = e.target.age.value;
+        const skill = e.target.skill.value;
+        const day = e.target.day.value;
+        const time = e.target.time.value;
+        const status ='Pending';
+        const applicantInfo = { name, email, photo, age, skill, day, time, status }
+        // console.log(applicantInfo)
+        axiosPublic.post('/appliedTrainer', applicantInfo)
+        .then(res=>{
+            console.log(res.data);
+            if(res.data.insertedId){
+                Swal.fire({
+                    icon: "success",
+                    title: "Submission Successfully",
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+            }
+        })
+        .catch(error => {
+            if (error.message) {
+                console.log(error.message);
+            }
 
+        })
     }
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/pzRtWs9/betrainer.jpg)' }}>
@@ -16,47 +47,65 @@ const BeTrainer = () => {
                     <Helmet><title>fitLab | BeTrainer</title></Helmet>
                     <h1 className="mb-5 text-5xl font-bold">Become a trainer</h1>
                     <div className="bg-opacity-20 my-16 text-base-content border-2  border-gray-400 card shrink-0  w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleBeTrainer} className="p-5 bg-gray-300 rounded-lg card-body space-y-5">
+                        <form onSubmit={handleBeTrainer} className="p-5 bg-gray-300 rounded-lg card-body ">
 
-                            <div className="form-control w-11/12 mx-auto ">
-                                <input name="name" type="name" placeholder="Your Name" defaultValue={user?.displayName} className="input input-bordered " required />
+                            <div className="form-control  ">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>                                <input name="name" type="name" placeholder="Your Name" defaultValue={user?.displayName} className="input input-bordered " required />
                             </div>
-                            <div className="form-control w-11/12 mx-auto">
+                            <div className="form-control ">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
                                 <input name="email" type="email" defaultValue={user?.email} className="input input-bordered" readOnly />
                             </div>
-                            <div className="form-control w-11/12 mx-auto">
+                            <div className="form-control ">
+                                <label className="label">
+                                    <span className="label-text">PhotoURL</span>
+                                </label>
                                 <input name="photo" type="photo" placeholder=" Your photoURL" defaultValue={user?.photoURL} className="input input-bordered" required />
                             </div>
-                            <div className="form-control w-11/12 mx-auto ">
+                            <div className="form-control  ">
+                                <label className="label">
+                                    <span className="label-text">Age</span>
+                                </label>
                                 <input name="age" type="age" placeholder="Your age" className="input input-bordered" required />
                             </div>
-                            <select name="skill" className="mx-auto select select-bordered w-full max-w-xs">
-                                <option disabled selected>Skills</option>
-                                <option>YOGA</option>
-                                <option>CARDIO BURN</option>
-                            </select>
-                            <div className="flex justify-center items-center gap-5 -ml-3">
-                                <div className="form-control w-11/12 mx-auto">
-                                    <div className="dropdown dropdown-hover">
-                                        <div tabIndex={0} role="button" className="btn m-1">Available Day</div>
-                                        <ul tabIndex={0} name="day" className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                            <li><a>Saturday</a></li>
-                                            <li><a>Sunday</a></li>
-                                            <li><a>Monday</a></li>
-                                            <li><a>Tuesday</a></li>
-                                            <li><a>wednesday</a></li>
-                                            <li><a>Thursday</a></li>
-                                            <li><a>Friday</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className="form-control w-11/12 mx-auto -ml-5">
-                                    <input name="time" type="time" placeholder="Available Time" className="input input-bordered" required />
-                                </div>
+                            <div className="form-control  ">
+                                <label className="label">
+                                    <span className="label-text">Skills</span>
+                                </label>
+                                <select name="skill" className="mx-auto select select-bordered w-full ">
+                                    <option disabled selected>Skills</option>
+                                    <option>YOGA</option>
+                                    <option>CARDIO BURN</option>
+                                </select>
                             </div>
-                            
-                            <div className="form-control w-11/12 mx-auto mt-6">
-                                <input type="submit" className="btn bg-[#D2B48C]" value="Apply Now" />
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Avaiable Day</span>
+                                </label>
+                                <select name="day" className="select select-bordered w-full ">
+                                    <option>Saturday</option>
+                                    <option>Sunday</option>
+                                    <option>Monday</option>
+                                    <option>Tuesday</option>
+                                    <option>wednesday</option>
+                                    <option>Thursday</option>
+                                    <option>Friday</option>
+                                </select>
+                            </div>
+
+                            <div className="form-control  ">
+                                <label className="label">
+                                    <span className="label-text">Time</span>
+                                </label>
+                                <input name="time" type="time" placeholder="Available Time" className="input input-bordered" required />
+                            </div>
+
+                            <div className="form-control  mt-6">
+                                <input type="submit" className="btn w-3/5 mx-auto bg-[#D2B48C]" value="Apply Now" />
                             </div>
                         </form>
                     </div>
