da8dced83cf347856ec01e0b37c9e75067d1d8d4
sujonifty <sujonifty@gmail.com>
2024-06-19 11:29:15 +0600

    jwt completed

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,9 +1,39 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const axiosSecure = axios.create({
     baseURL: 'https://fit-lab-server-side.vercel.app'
 })
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logOut } = useContext(AuthContext);
+
+    // request interceptor to add authorization header for every secure call to teh api
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+
+    // intercepts 401 and 403 status
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, async (error) => {
+        const status = error.response.status;
+        // for 401 or 403 logout the user and move the user to the login
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
+        }
+        return Promise.reject(error);
+    })
+
     return axiosSecure;
 };
 
